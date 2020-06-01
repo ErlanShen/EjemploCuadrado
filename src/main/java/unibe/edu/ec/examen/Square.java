7f566fbd099a13be240973ceb15979e9fe315186
O_o-X <O_o-X@Ban-Nox>
2020-06-01 11:44:13 -0500

    Cualquier cambio

--- src/main/java/unibe/edu/ec/examen/Square.java
+++ src/main/java/unibe/edu/ec/examen/Square.java
@@ -1,5 +1,23 @@
 package unibe.edu.ec.examen;
 
 public class Square {
+	private double side;
 
+	public Square(double side) {
+		super();
+		this.side = side;
+	}
+
+	public double getSide() {
+		return side;
+	}
+
+	public void setSide(double side) {
+		this.side = side;
+	}
+
+	public void CalculateArea() {
+		double area= side*side*side*side;
+		return area;
+	}
 }
